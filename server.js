77b39be00afa1c5adb71e15036fb0dcd2c16a399
Pallop <jibbiw123456@gmail.com>
2020-09-03 19:52:02 +0700

    bug hiroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/doscg'));
+app.use(express.static(__dirname + '/dist/DOSCG'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/doscg/index.html'));});
+'/dist/DOSCG/index.html'));});
+
+console.log("Listening on " + (process.env.PORT || 4200));
+
 app.listen(process.env.PORT || 4200);
